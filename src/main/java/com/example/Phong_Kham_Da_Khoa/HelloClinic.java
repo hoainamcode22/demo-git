4dec3828721e96a3ace54ae0049acfa814ec0419
hoainamcode <nam695472@gmail.com>
2025-07-27 17:35:50 +0700

    Thêm file HelloClinic.java

---
+++ src/main/java/com/example/Phong_Kham_Da_Khoa/HelloClinic.java
@@ -0,0 +1,7 @@
+package com.example.Phong_Kham_Da_Khoa;
+
+public class HelloClinic {
+    public static void main(String[] args) {
+        System.out.println("Chào mừng đến với Phòng Khám Đa Khoa!");
+    }
+}
