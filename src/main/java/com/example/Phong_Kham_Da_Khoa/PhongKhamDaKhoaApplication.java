423fd32cb34198b3944db3a5d2f312c17d9695a0
hoainamcode <nam695472@gmail.com>
2025-07-27 17:19:06 +0700

    Initial commit

---
+++ src/main/java/com/example/Phong_Kham_Da_Khoa/PhongKhamDaKhoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phong_Kham_Da_Khoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongKhamDaKhoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhongKhamDaKhoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Phong_Kham_Da_Khoa/PhongKhamDaKhoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Phong_Kham_Da_Khoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhongKhamDaKhoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
